e406777e2cabb5fad25fdaaa53fc77674811e2f9
Kyle Duffy <kyleduffy83@gmail.com>
2021-10-30 10:03:52 -0400

    Update globals.js

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,5 @@
 module.exports = {
     // db: 'mongodb://localhost/RamaraHSDB' // local mongodb
-db: 'mongodb://kduffy93:Hjaalmarch11@ds229415.mlab.com:29415/ramara-health-and-safety'
+//db: 'mongodb://kduffy93:Hjaalmarch11@ds229415.mlab.com:29415/ramara-health-and-safety'
+    mongodb+srv://kduffy:<Hjaalmarch11>@cluster0.tzgkf.mongodb.net/RamaraAppDb?retryWrites=true&w=majority
 };
