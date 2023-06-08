a6bb883f69acd2da5dd3761b12a3015ffde4f602
Kyle Duffy <kyleduffy83@gmail.com>
2023-06-08 04:15:11 -0400

    remote db connection

--- config/globals.js
+++ config/globals.js
@@ -3,5 +3,5 @@ module.exports = {
 //db: 'mongodb://kduffy93:Hjaalmarch11@ds229415.mlab.com:29415/ramara-health-and-safety'
     //mongodb+srv://kduffy:<Hjaalmarch11>@cluster0.tzgkf.mongodb.net/RamaraAppDb?retryWrites=true&w=majority
      //db: 'mongodb://kduffy:Haafingar11@cluster0-shard-00-00.tzgkf.mongodb.net:27017,cluster0-shard-00-01.tzgkf.mongodb.net:27017,cluster0-shard-00-02.tzgkf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-3ln9ki-shard-0&authSource=admin&retryWrites=true&w=majority'
-db: 'mongodb://localhost:27017/testdb'
+db: 'mongodb://74.116.223.113:27017/testdb'
 };
